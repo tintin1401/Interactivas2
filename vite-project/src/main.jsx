aa82cce7aaf6f457b9bf18c273f6043f8ad19ff1
Dana Valverde <dana.cardenascr@gmail.com>
2024-04-21 22:49:42 -0600

    Possible Calendar

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom/client'
 import { Card } from './Card.jsx'
 import { Login } from './Login.jsx'
 import { Register } from './register.jsx'
-import { Calendar } from './Calendar.jsx'
+import { Practice } from './Practice.jsx'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <Calendar/>
+    <Practice/>
    
 
 
