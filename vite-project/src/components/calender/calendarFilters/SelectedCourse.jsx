e2bbc332a29ae12e01faed4ca0079447f800e4d6
Nahomi27 <lawrencenahomi@gmail.com>
2024-05-04 02:24:30 -0600

    Filters for the calendar

---
+++ vite-project/src/components/calender/calendarFilters/SelectedCourse.jsx
@@ -0,0 +1,30 @@
+import "../../../index.css";
+import React, { useState } from 'react';
+
+export function SelectedCourse() {
+    const [selectedItem, setSelectedItem] = useState('');
+  
+    const handleSelectChange = (event) => {
+      setSelectedItem(event.target.value);
+    };
+
+    const courses = ['Aplicaciones Interactivas', 'Diseño Web', 'Ingenieria', 'Manipulacion de Audio y Video'];
+  
+    return (
+      <div className="border-2 border-blue-60000 p-4 rounded-2xl ff-main">
+        <h2 className="font-bold mb-3">Courses</h2>
+        <select value={selectedItem} onChange={handleSelectChange} className="text-gray-600 block w-full border-none outline-none">
+
+          <option value="">Choose a course</option>
+          {courses.map((courses, index) => (
+            <option key={index} value={courses}>
+              {courses}
+            </option>
+          ))}
+        </select>
+
+        <hr className="border border-blue-600" />
+
+      </div>
+    );
+  }
--- vite-project/src/components/cards/EventCardCta.jsx
+++ vite-project/src/components/cards/EventCardCta.jsx
@@ -1,5 +1,7 @@
 import { Schedule } from "./Schedule"
 import { Calendar } from "../calender/Calendar";
+import { SelectedCourse } from "../calender/calendarFilters/SelectedCourse";
+import { TagCategories } from "../calender/calendarFilters/TagCategories";
 import "../../index.css";
 
 export function EventCardCta() {
@@ -8,30 +10,37 @@ export function EventCardCta() {
     ]
 
     return (
-        <section class="grid lg:grid-cols-2 justify-center items-center h-screen lg:gap-4  gap-[15rem]">
-        <section class="flex flex-col bg-white rounded-3xl">
-            <div class="flex justify-center  text-[2rem] text-blue-600 h-[7rem] items-center font-black ff-main">
-                <h1 class="text-center">Schedule</h1>
-            </div>
-            <div class="flex col-start-1 col-end-3 ml-[2rem] lg:col-start-2 lg:col-end-4 gap-4 lg:ml-[5rem] font-bold ff-main">
-                <h2 class="hover:underline hover:decoration-4 cursor-pointer decoration-[#3262DE]">Event</h2>
-                <h2 class="hover:underline hover:decoration-4 cursor-pointer decoration-[#3262DE]">Task</h2>
-                <h2 class="hover:underline hover:decoration-4 cursor-pointer decoration-[#3262DE]">Courses</h2>
-            </div>
-            <div className="scroll scrollbar-thumb-orange-700 scrollbar-track-white overflow-y-scroll mb-8 lg:scroll-smooth lg:scrollbar-thin p-4 lg:p-10 h-[75vh]">
-                <section className="grid gap-4">
-                    <Schedule items={events} />
-                    <Schedule items={events} />
-                    <Schedule items={events} />
-                    <Schedule items={events} />
+        <div className="bg-blue-100">
+            <section className="grid lg:grid-cols-2 justify-center items-center h-screen gap-4">
+                <section className="flex flex-col bg-white rounded-3xl">
+                    <div className="flex justify-center text-[2rem] text-blue-600 h-[7rem] items-center font-black ff-main">
+                        <h1 className="text-center">Schedule</h1>
+                    </div>
+                    <div className="flex col-start-1 col-end-3 ml-[2rem] lg:col-start-2 lg:col-end-4 gap-4 lg:ml-[5rem] font-bold ff-main">
+                        <h2 className="hover:underline hover:decoration-4 cursor-pointer decoration-[#3262DE]">Event</h2>
+                        <h2 className="hover:underline hover:decoration-4 cursor-pointer decoration-[#3262DE]">Task</h2>
+                        <h2 className="hover:underline hover:decoration-4 cursor-pointer decoration-[#3262DE]">Courses</h2>
+                    </div>
+                    <div className="scroll scrollbar-thumb-orange-700 scrollbar-track-white overflow-y-scroll mb-8 lg:scroll-smooth lg:scrollbar-thin p-4 lg:p-10 h-[75vh]">
+                        <section className="grid gap-4">
+                            <Schedule items={events} />
+                            <Schedule items={events} />
+                            <Schedule items={events} />
+                            <Schedule items={events} />
+                        </section>
+                    </div>
                 </section>
-            </div>
-        </section>
 
-        <Calendar />
-
-
-    </section>
+                <section className="flex flex-col bg-white rounded-3xl">
+                    <div className="p-8">
+                        <SelectedCourse/>
+                        <TagCategories/>
+                        <Calendar />
+                    </div>
+                </section>
+            </section>
+        </div>
+        
     );
 }
 
