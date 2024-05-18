14324275e6a57661c6b33bd775e6d8e5f69830d4
TaylorYT29 <taylorduransibaja@gmail.com>
2024-05-17 18:18:17 -0700

    ChangePassWord className

--- vite-project/src/components/profile/ChangePassword.jsx
+++ vite-project/src/components/profile/ChangePassword.jsx
@@ -24,31 +24,31 @@ export function ChangePassword() {
                 transition={{ duration: 0.5 }}
             >
                 <div className="bg-white rounded-3xl pl-8 pr-8">
-                    <div class="grid justify-center m-4">
+                    <div className="grid justify-center m-4">
                         <form>
-                            <div class="grid gap-6 mb-6 md:grid-cols-1">
+                            <div className="grid gap-6 mb-6 md:grid-cols-1">
                                 <h1 className='font-bold text-blue-700 text-3xl text-center mb-4'>Change Password</h1>
-                                <div class="mb-3">
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Old password</label>
-                                    <input type="password" id="password" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="•••••••••" required />
+                                <div className="mb-3">
+                                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Old password</label>
+                                    <input type="password" id="password" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="•••••••••" required />
                                 </div>
 
-                                <div class="mb-3">
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900">New password</label>
-                                    <input type="password" id="password" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="•••••••••" required />
+                                <div className="mb-3">
+                                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900">New password</label>
+                                    <input type="password" id="password" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="•••••••••" required />
                                 </div>
 
-                                <div class="mb-3">
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Write it again</label>
-                                    <input type="password" id="password" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="•••••••••" required />
+                                <div className="mb-3">
+                                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Write it again</label>
+                                    <input type="password" id="password" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="•••••••••" required />
                                 </div>
 
 
                             </div>
 
                             <div className='grid gap-6 lg:grid-cols-2 grid-cols-1 w-[20rem] pb-2 pt-2'>
-                                <button type="submit" class="text-black border-2 border-orange-700 p-2 rounded-lg">Cancel</button>
-                                <button type="submit" class="text-white bg-orange-700 p-2 rounded-lg">Change</button>
+                                <button type="submit" className="text-black border-2 border-orange-700 p-2 rounded-lg">Cancel</button>
+                                <button type="submit" className="text-white bg-orange-700 p-2 rounded-lg">Change</button>
                             </div>
 
 
