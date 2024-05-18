1c960eb30956fde728973caa95f8a949758ebefa
Nahomi27 <lawrencenahomi@gmail.com>
2024-05-17 18:16:09 -0600

    UserProfile

--- vite-project/src/components/routes/UserProfile.jsx
+++ vite-project/src/components/routes/UserProfile.jsx
@@ -40,66 +40,66 @@ export function UserProfile() {
                         transition={{ duration: 0.5, delay: 1 }}
                     >
                         <div className="bg-white rounded-3xl pl-8 pr-8">
-                            <div class="grid justify-center m-4">
+                            <div className="grid justify-center m-4">
                                 <form>
                                     <h1 className='font-bold text-blue-700 text-3xl mb-2'>Account Settings</h1>
                                     <div className="flex items-center gap-4 mb-8">
                                         <img className='block rounded-[50%] w-20' src="https://randomuser.me/api/portraits/women/31.jpg" alt="User Profile" />
                                         <p className='text-md font-medium'>User Name</p>
                                     </div>
-                                    <div class="flex flex-col gap-6 mb-1 md:flex-row">
-                                        <div class="">
-                                            <label for="fullName" class="block mb-1 text-sm font-medium text-gray-900">Full name</label>
-                                            <input type="fullName" id="fullName" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="Full name" />
+                                    <div className="flex flex-col gap-6 mb-1 md:flex-row">
+                                        <div className="">
+                                            <label for="fullName" className="block mb-1 text-sm font-medium text-gray-900">Full name</label>
+                                            <input type="fullName" id="fullName" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="Full name" />
                                         </div>
 
-                                        <div class="mb-4">
-                                            <label for="carnet" class="block mb-1 text-sm font-medium text-gray-900">Carnet</label>
-                                            <input type="carnet" id="carnet" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="Carnet" />
+                                        <div className="mb-4">
+                                            <label for="carnet" className="block mb-1 text-sm font-medium text-gray-900">Carnet</label>
+                                            <input type="carnet" id="carnet" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="Carnet" />
                                         </div>
                                     </div>
                                     <div className="flex flex-col gap-6 mb-2 md:flex-row">
-                                        <div class="">
-                                            <label for="email" class="block mb-1 text-sm font-medium text-gray-900">Email</label>
-                                            <input type="email" id="email" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="@gmail.com" />
+                                        <div className="">
+                                            <label for="email" className="block mb-1 text-sm font-medium text-gray-900">Email</label>
+                                            <input type="email" id="email" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="@gmail.com" />
                                         </div>
-                                        <div class="mb-6">
-                                            <label for="password" class="block mb-1 text-sm font-medium text-gray-900">Password</label>
-                                            <input type="password" id="password" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="•••••••••" required />
+                                        <div className="mb-6">
+                                            <label for="password" className="block mb-1 text-sm font-medium text-gray-900">Password</label>
+                                            <input type="password" id="password" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" placeholder="•••••••••" required />
                                         </div>
                                     </div>
 
                                     <h2 className='font-bold text-blue-700 text-3xl mb-2'>Personal Information</h2>
-                                    <div class="flex flex-col gap-6 mb-1 md:flex-row">
+                                    <div className="flex flex-col gap-6 mb-1 md:flex-row">
 
-                                        <div class="">
-                                            <label for="hoursOfSleep" class="block mb-1 text-sm font-medium text-gray-900">Hours of sleep</label>
-                                            <select id="hoursOfSleep" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" required>
+                                        <div className="">
+                                            <label for="hoursOfSleep" className="block mb-1 text-sm font-medium text-gray-900">Hours of sleep</label>
+                                            <select id="hoursOfSleep" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" required>
                                                 <option value="yes">Less than 4 hours</option>
                                                 <option value="no">4 to 6 hours</option>
                                                 <option value="no">6 to 8 hours</option>
                                                 <option value="no">More than 8 hours</option>
                                             </select>
                                         </div>
-                                        <div class="mb-4">
-                                            <label for="hoursOfSleep" class="block mb-1 text-sm font-medium text-gray-900">Physical activity</label>
-                                            <select id="hoursOfSleep" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" required>
+                                        <div className="mb-4">
+                                            <label for="hoursOfSleep" className="block mb-1 text-sm font-medium text-gray-900">Physical activity</label>
+                                            <select id="hoursOfSleep" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" required>
                                                 <option value="yes">Yes</option>
                                                 <option value="no">No</option>
                                             </select>
                                         </div>
                                     </div>
                                     <div className="flex flex-col gap-6 mb-6 md:flex-row">
-                                        <div class="">
-                                            <label for="diseases" class="block mb-1 text-sm font-medium text-gray-900">Diseases</label>
-                                            <select id="diseases" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" required>
+                                        <div className="">
+                                            <label for="diseases" className="block mb-1 text-sm font-medium text-gray-900">Diseases</label>
+                                            <select id="diseases" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" required>
                                                 <option value="yes">Yes</option>
                                                 <option value="no">No</option>
                                             </select>
                                         </div>
-                                        <div class="mb-4">
-                                            <label for="diseases" class="block mb-1 text-sm font-medium text-gray-900">Gender</label>
-                                            <select id="diseases" class="border-2 border-blue-700 rounded-md w-[20rem] p-2" required>
+                                        <div className="mb-4">
+                                            <label for="diseases" className="block mb-1 text-sm font-medium text-gray-900">Gender</label>
+                                            <select id="diseases" className="border-2 border-blue-700 rounded-md w-[20rem] p-2" required>
                                                 <option value="yes">Woman</option>
                                                 <option value="no">Men</option>
                                                 <option value="no">No specified</option>
@@ -108,8 +108,8 @@ export function UserProfile() {
                                     </div>
 
                                     <div className='grid gap-6 lg:grid-cols-2 grid-cols-1 w-[20rem] pb-2'>
-                                        <button type="submit" class="text-black border-2 border-orange-700 p-2 rounded-lg">Cancel</button>
-                                        <button type="submit" class="text-white bg-orange-700 p-2 rounded-lg">Save</button>
+                                        <button type="submit" className="text-black border-2 border-orange-700 p-2 rounded-lg">Cancel</button>
+                                        <button type="submit" className="text-white bg-orange-700 p-2 rounded-lg">Save</button>
                                     </div>
 
                                 </form>
