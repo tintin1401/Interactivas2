55f04952239adce310f57caa385d3ed92aea066d
TaylorYT29 <taylorduransibaja@gmail.com>
2024-06-08 20:34:02 -0700

    repair

--- vite-project/src/components/routes/Home.jsx
+++ vite-project/src/components/routes/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import ToggleSidebar from '../layout/sidebar/ToggleSidebar.jsx';
 import Sidebar from '../layout/sidebar/Sidebar.jsx';
-import { CtaContainer } from '../containers/CtaContainer.jsx'; 
-import { Index } from '../ui/landing/Index.jsx'; 
-import { Schedule } from '../cards/Schedule.jsx'; 
+import { CtaContainer } from '../containers/CtaContainer.jsx';
+import { Index } from '../ui/landing/Index.jsx';
+import { Schedule } from '../cards/Schedule.jsx';
 import { motion } from "framer-motion";
 import { useHome } from '../hooks/useHome.js';
+import useFetchData from "../hooks/useFetchData.js";
 
 /**
  * Renders the Home component.
@@ -15,19 +16,28 @@ import { useHome } from '../hooks/useHome.js';
 function Home() {
     const { sidebarToggle, isSidebarVisible, isCtaVisible, isIndexVisible, isScheduleVisible, setSidebarToggle } = useHome();
 
-    const events = [
-        { "id": 1, "image": "https://images.squarespace-cdn.com/content/v1/60794dbc8615125d3ad57026/adf94af6-7aab-4503-9569-527b9faec977/react1logo.png", "title": "Task", "date": "August 30, 2024", "event": "Event", "hour": "5:00 p.m.", "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quod earum, nemo amet sint?.", },
-        { "id": 2, "image": "https://images.squarespace-cdn.com/content/v1/60794dbc8615125d3ad57026/adf94af6-7aab-4503-9569-527b9faec977/react1logo.png", "title": "Task", "date": "August 30, 2024", "event": "Event", "hour": "5:00 p.m.", "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quod earum, nemo amet sint?.", },
-        {
-            "id": 3, "image": "https://images.squarespace-cdn.com/content/v1/60794dbc8615125d3ad57026/adf94af6-7aab-4503-9569-527b9faec977/react1logo.png", "title": "Task", "date": "August 30, 2024", "event": "Event", "hour": "5:00 p.m.", "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quod earum, nemo amet sint?.",
-        }, {
-            "id": 4, "image": "https://images.squarespace-cdn.com/content/v1/60794dbc8615125d3ad57026/adf94af6-7aab-4503-9569-527b9faec977/react1logo.png", "title": "Task", "date": "August 30, 2024", "event": "Event", "hour": "5:00 p.m.", "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quod earum, nemo amet sint?.",
-        }, {
-            "id": 5, "image": "https://images.squarespace-cdn.com/content/v1/60794dbc8615125d3ad57026/adf94af6-7aab-4503-9569-527b9faec977/react1logo.png", "title": "Task", "date": "August 30, 2024", "event": "Event", "hour": "5:00 p.m.", "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quod earum, nemo amet sint?.",
-        }, {
-            "id": 6, "image": "https://images.squarespace-cdn.com/content/v1/60794dbc8615125d3ad57026/adf94af6-7aab-4503-9569-527b9faec977/react1logo.png", "title": "Task", "date": "August 30, 2024", "event": "Event", "hour": "5:00 p.m.", "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quod earum, nemo amet sint?.",
-        },
-    ]
+    const [value, setValue] = useState(0);
+    const [url, setUrl] = useState("http://localhost/calenderbackend/public/api/activities/all");
+    const urltest = value === 0
+        ? "http://localhost/calenderbackend/public/api/activities/all"
+        : `http://localhost/calenderbackend/public/api/activities/findcourses/${value}`;
+    setUrl
+    const { data, loading } = useFetchData(url);
+
+    useEffect(() => {
+        addEvent(value);
+
+    }, []);
+
+
+    const addEvent = (value) => {
+
+        setValue(value);
+    };
+
+
+   
+
 
     return (
         <div className="flex">
@@ -68,7 +78,11 @@ function Home() {
                     {/*<ScheduleContainer />*/}
                     <h2 className="ff-main text-2xl font-bold pb-[1rem] px-[1rem] pt-[1.5rem] ml-10  text-blue-600">Events</h2>
                     <div className="grid xl:grid-cols-[repeat(2,minmax(0,1fr))] gap-5 mx-5">
-                        <Schedule/>
+                        {loading ? (
+                            <p value="">loading</p>
+                        ) : (
+                            <Schedule activities={data} />
+                        )}
                     </div>
                 </motion.div>
             </main>
