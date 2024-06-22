be7c66c329c45a381c7722bd622cc4bfcfd3f67b
JR095 <jurgenbs01@gmail.com>
2024-06-21 20:12:49 -0600

    :)

--- vite-project/src/components/hooks/useFetchSearch.js
+++ vite-project/src/components/hooks/useFetchSearch.js
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react';
-
+import { useUser } from '../../context/UserContext.jsx';
 const useFetchSearch = (searchTerm) => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
+    const { user } = useUser();
 
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
             try {
-                const response = await fetch(`http://localhost/calenderbackend/public/api/activities/search/?name=${searchTerm}`);
+                const response = await fetch(`http://localhost/calenderbackend/public/api/activities/search/${user.id}?name=${searchTerm}`);
                 const result = await response.json();
                 setData(result);
             } catch (error) {
