3efb179746eb9622e734531e65e86db74d6eee3b
JR095 <jurgenbs01@gmail.com>
2024-06-21 21:26:31 -0600

    Minor change

--- vite-project/src/components/containers/CtaContainer.jsx
+++ vite-project/src/components/containers/CtaContainer.jsx
@@ -1,6 +1,7 @@
 import "../../index.css";
 import PropTypes from 'prop-types';
 import ctaImg from "../../assets/imgs/cta-img.png";
+import { useUser } from '../../context/UserContext.jsx';
 /**
  * Renders a call-to-action container component with the given user name, task number, and button.
  *
@@ -11,10 +12,11 @@ import ctaImg from "../../assets/imgs/cta-img.png";
  * @return {JSX.Element} The rendered call-to-action container component.
  */
 export function CtaContainer({userName, taskNumber, button}){
+  const { user } = useUser();
     return(
       <section className='bg-blue-900 flex justify-between gap-8 px-16 rounded-[3rem] text-white items-center'>
       <section className='my-8 lg:ml-10 md:ml-10 ml-0 ff-main'>
-        <h2 className='font-medium text-blue-100'>Hi {userName}!</h2>
+        <h2 className='font-medium text-blue-100'>Hi {user.name}!</h2>
         <p className="lg:text-4xl text-lg mt-5 font-medium max-w-[16ch]">You have completed {taskNumber} task this week!</p>
 
         <div className="mt-4">
